a60de5e062e3ae21d851e7d774369a0cf74a3f27
user <juan.zuluaga4461@uco.net.co>
2024-04-15 18:37:45 -0500

    first commit

---
+++ src/test/java/com/quind/prueba/tecnica/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quind.prueba.tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
